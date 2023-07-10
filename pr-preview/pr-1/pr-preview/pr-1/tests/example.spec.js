715767bf5e1678b3bade38487085269be3829c24
ahmedelkolfat <ahmedelkolfat@users.noreply.github.com>
2023-07-10 03:42:53 +0000

    Remove preview for PR 1 🛬

--- pr-preview/pr-1/pr-preview/pr-1/tests/example.spec.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  await expect(page).toHaveTitle(/Playwright/);
-});
